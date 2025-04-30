2cd8e838a8a38b14175a2c03aef6061d4ba594d5
Blake Newhouse <56932104+blakenewhouse@users.noreply.github.com>
2025-04-29 17:42:34 -0700

    Update lab4.js

--- scripts/lab4.js
+++ scripts/lab4.js
@@ -26,7 +26,7 @@ function sumValues(num1, num2, add) {
  */
 function discountPrices(prices, discount) {
     const discounted = []
-    const length = prices.length;
+    const length = prices.length();
 
     if (length == 0) {
         return false;
